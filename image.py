5e3ad50179b2febda537ae08e59811b477dd607f
Kanishk Rastogi <kanishkrastogi46@gmail.com>
2023-04-10 19:41:32 +0530

    added mainframe in my_image

--- image.py
+++ image.py
@@ -11,7 +11,7 @@ mainframe.grid(row=0, column=0, sticky=(N,W,E,S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
-my_image= ImageTk(Image.open("download.jpeg"))
-my_label= ttk.Label(mainframe, image=my_image)
+my_image= ImageTk(mainframe, Image.open("download.jpeg"))
+my_label= ttk.Label(image=my_image)
 
 root.mainloop()
